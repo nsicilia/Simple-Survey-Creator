c17cace36224a226baf4ba6d4a9fb87f6ea8f2dd
Nicholas Siciliano <nsicilia@oswego.edu>
2022-10-09 18:29:55 -0400

    Dynamically add questions

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ function App() {
   var [questions, setQuestions] = useState([]);
   var [count, setCount] = useState(0);
 
+  var [title, setTitle] = useState("");
+  var [type, setType] = useState("text");
+
+  var [option1, setOption1] = useState("");
+  var [option2, setOption2] = useState("");
+  var [option3, setOption3] = useState("");
+  var [option4, setOption4] = useState("");
 
   const addQuestion = () => {
     setCount((prevState) => prevState + 1);
     setQuestions((prevState) => {
-      const newQuestions = Array.from(prevState);  // CREATING A NEW ARRAY OBJECT
-      questions.push(count);
+      const newQuestions = Array.from(prevState); // copy the array
+      //questions.push({index: count, title: title, type: type, option1: option1, option2: option2, option3: option3, option4: option4});
+      questions.push({ index: count, title: title, type: type, options: [option1, option2, option3, option4] });
       return newQuestions;
     });
   };
+
+
 
   const removeQuestion = (test) => {
     //console.log(test);
@@ -24,6 +34,7 @@ function App() {
       return newQuestions;
     });
   };
+
 
 
   const moveQuestion = (question, direction) => {
@@ -60,24 +71,80 @@ function App() {
   return (
     <div>
       <h1>Simple Question UI</h1>
-      <button className="button" onClick={addQuestion}>
-        +
-      </button>
+      <div style={{ border: "5px solid red" }}>
+        <h1>Create Question</h1>
+
+        <label>Question</label>
+        <input
+          type="text"
+          placeholder="Enter Question"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+
+        <label>Question Type</label>
+        <select
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="text">Text</option>
+          <option value="bool">Boolean</option>
+          <option value="number">Number</option>
+          <option value="multiple">Multiple Choice</option>
+        </select>
+
+        {type === "multiple" ? (
+          <div>
+
+            <label>Choose 1</label>
+            <input type="text" placeholder="Enter Option 1" onChange={(e) => setOption1(e.target.value)} />
+
+            <label>Choose 2</label>
+            <input type="text" placeholder="Enter Option 2" onChange={(e) => setOption2(e.target.value)} />
+
+            <label>Choose 3</label>
+            <input type="text" placeholder="Enter Option 3" onChange={(e) => setOption3(e.target.value)} />
+
+            <label>Choose 4</label>
+            <input type="text" placeholder="Enter Option 4" onChange={(e) => setOption4(e.target.value)} />
+
+          </div>
+        ): null
+        }
+
+        {type === "bool" ? (
+          <div>
+
+            <label>Choose 1 (ex. true)</label>
+            <input type="text" placeholder="Enter Option 1" onChange={(e) => setOption1(e.target.value)} />
+
+            <label>Choose 2(ex. false)</label>
+            <input type="text" placeholder="Enter Option 2" onChange={(e) => setOption2(e.target.value)} />
+
+          </div>
+        ): null
+        }
+
+        <button className="button" onClick={addQuestion}>
+          +
+        </button>
+      </div>
+
+
 
       <div>
-        {questions.map((index) => (
-          <div key={index}>
-            {QuestionBox(index)}
+        {questions.map((question) => (
+          <div key={question.index}>
+            {QuestionBox(question)}
 
-            <button className="button" onClick={() => removeQuestion(index)}>
+            <button className="button" onClick={() => removeQuestion(question)}>
               -
             </button>
-             
-            <button className="button" onClick={() => moveQuestion(index, 'up')}>
+
+            <button className="button" onClick={() => moveQuestion(question, 'up')}>
               ^
             </button>
-              
-            <button className="button" onClick={() => moveQuestion(index, 'down')}>
+
+            <button className="button" onClick={() => moveQuestion(question, 'down')}>
               v
             </button>
           </div>
--- src/Components/inputField/QuestionBox.js
+++ src/Components/inputField/QuestionBox.js
@@ -1,15 +1,62 @@
 import React from 'react'
 import './classes.css'
 
-const QuestionBox = (num) => {
+const QuestionBox = ({title, type, options}) => {
+
+  function renderSwitch(type) {
+    switch (type) {
+      case 'text':
+        return (
+          <label>Enter your name:
+            <input type="text" />
+          </label>
+        );
+
+      case 'bool':
+        return (
+          <>
+          <button className="button">{options[0]}</button>
+  
+          <button className="button">{options[1]}</button>
+          </>
+        );
+
+      case 'number':
+        return 'this is the number ';
+
+      case 'multiple':
+        return (
+          <>
+          <label>
+          <input type="radio" value="Male" name="gender"/>
+          {options[0]}
+          </label>
+          <label>
+          <input type="radio" value="Male" name="gender"/>
+          {options[1]}
+          </label>
+          <label>
+          <input type="radio" value="Male" name="gender"/>
+          {options[2]}
+          </label>
+          <label>
+          <input type="radio" value="Male" name="gender"/>
+          {options[3]}
+          </label>
+          </>
+        );
+
+      default:
+        return 'Error';
+    }
+  };
+
   return (
     <div className="container">
-        <form>
-          <h1>{num}</h1>
-            <label>Enter your name:
-            <input type="text" />
-            </label>
-        </form>
+
+      <h1>{title}</h1>
+      {renderSwitch(type)}
+
     </div>
   )
 }
